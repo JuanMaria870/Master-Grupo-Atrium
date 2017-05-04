ef836b12bf9463144281a0b9b308ca33a278d701
juan <juaniros_note@hotmail.com>
2017-05-04 21:45:25 +0200

    EntregableTema3

---
+++ EntregableTema3/src/main/java/com/grupoatrium/modelo/Editorial.java
@@ -0,0 +1,83 @@
+package com.grupoatrium.modelo;
+
+public class Editorial {
+
+	String nombre;
+	Direccion direccion;
+	String nif;
+
+	/**
+	 * 
+	 */
+	public Editorial() {
+		super();
+	}
+
+	/**
+	 * @param nombre
+	 * @param direccion
+	 * @param nif
+	 */
+	public Editorial(String nombre, Direccion direccion, String nif) {
+		super();
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.nif = nif;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre
+	 *            the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the direccion
+	 */
+	public Direccion getDireccion() {
+		return direccion;
+	}
+
+	/**
+	 * @param direccion
+	 *            the direccion to set
+	 */
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+
+	/**
+	 * @return the nif
+	 */
+	public String getNif() {
+		return nif;
+	}
+
+	/**
+	 * @param nif
+	 *            the nif to set
+	 */
+	public void setNif(String nif) {
+		this.nif = nif;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Editorial [nombre=" + nombre + ", direccion=" + direccion + ", nif=" + nif + "]";
+	}
+
+}
